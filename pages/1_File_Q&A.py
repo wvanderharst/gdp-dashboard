c57d24fa6292c458fc35424b12c2cf1e2753a1f4
wvanderharst <90245369+wvanderharst@users.noreply.github.com>
2024-10-04 14:41:12 +0200

    Add files via upload

---
+++ pages/1_File_Q&A.py
@@ -0,0 +1,76 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import ast
+import itertools
+
+st.title("Meest voorkomende combinates")
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("https://raw.githubusercontent.com/wvanderharst/peilingen-dataset/refs/heads/main/data/coalitions.csv")
+    return df
+
+def load_data2():
+    df = pd.read_csv("https://raw.githubusercontent.com/wvanderharst/peilingen-dataset/refs/heads/main/data/partygov.csv")
+    return df
+
+def load_data3():
+    df = pd.read_csv("https://raw.githubusercontent.com/wvanderharst/peilingen-dataset/refs/heads/main/data/partygovmultiple.csv")
+    return df
+
+
+
+def all_in_list(short_list, long_list):
+    return set(short_list).issubset(set(long_list))
+
+def convert_string_to_list(s):
+    return ast.literal_eval(s)
+
+def contains_all(long_list, short_list):
+    return all(item in long_list for item in short_list)
+
+
+df = load_data()
+df_multiple = load_data3()
+#df2['reger'] = df2['reger'].str.replace('(', '').str.replace(')', '').str.replace(',', '').str.replace("'", '')
+
+df4 = pd.read_csv("https://raw.githubusercontent.com/wvanderharst/peilingen-dataset/refs/heads/main/data/partygov.csv")
+
+allparties = set(df4["Partij"])
+
+
+df['reger'] = df['reger'].apply(convert_string_to_list)
+df_multiple["Key"] = df_multiple["Key"].apply(convert_string_to_list)
+st.dataframe(
+    df['reger'].value_counts())
+
+
+
+
+df3 = load_data2()
+
+# Show a multiselect widget with the genres using `st.multiselect`.
+partij = st.multiselect(
+    "Partij",
+    df3.Partij.unique(),
+    ["PVV"],
+)
+
+
+
+df_multiple = df_multiple.sort_values(by='Value', ascending=False)
+df_multiple2 = df_multiple.sort_values(by='Value', ascending=False)
+# Reset the index if desired
+#df_multiple = df_multiple.reset_index(drop=True, inplace=True)
+
+df_multiple = df_multiple[df_multiple['Key'].apply(lambda x: contains_all(x, partij))]
+df_multiple= df_multiple[df_multiple['Value'] != 0]
+st.dataframe(
+    df_multiple)
+
+
+df_filtered = df_multiple2[df_multiple2['Key'].apply(lambda x: len(x) <= 1)]
+
+st.dataframe(df_filtered)
---
+++ pages/2_ Voting_Power.py
@@ -0,0 +1,68 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import ast
+import itertools
+
+
+
+def all_in_list(short_list, long_list):
+    return set(short_list).issubset(set(long_list))
+
+def convert_string_to_list(s):
+    return ast.literal_eval(s)
+
+def contains_all(long_list, short_list):
+    return all(item in long_list for item in short_list)
+
+def load_data2():
+    df = pd.read_csv("https://raw.githubusercontent.com/wvanderharst/peilingen-dataset/refs/heads/main/data/partygov.csv")
+    return df
+
+def load_data3():
+    df = pd.read_csv("https://raw.githubusercontent.com/wvanderharst/peilingen-dataset/refs/heads/main/data/elections.csv")
+    return df
+
+length = load_data3()
+lengthN = len(length.columns)-2
+
+df3 = load_data2()
+
+# Show a multiselect widget with the genres using `st.multiselect`.
+partij = st.selectbox(
+    "Partij",
+    df3.Partij.unique())
+partij1 = partij.replace('/', '')
+
+df = pd.read_csv(f'https://raw.githubusercontent.com/wvanderharst/peilingen-dataset/refs/heads/main/data_conditional/{partij1}.csv')
+
+allparties = set(df3["Partij"])
+
+
+df['reger'] = df['reger'].apply(convert_string_to_list)
+
+
+df34= df[df['reger'].apply(lambda x: partij in x)]
+
+st.dataframe(
+    df['reger'].value_counts()/len(df['reger']))
+
+df8 = df['reger'].value_counts().sum()
+df34 = df34['reger'].value_counts().sum()
+#df8 = (df['reger'].value_counts()/len(df['reger'])).sum()
+#df34 = (df34['reger'].value_counts()/len(df34['reger'])).sum()
+num = df34/df8
+labels = 'Coaltitie', 'Oppositie'
+sizes = [num, 1-num ]
+explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+
+
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+    shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.title(partij)
+st.pyplot(fig1)
+
